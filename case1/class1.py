057c320b6dc2a4756c40dc566bc49185a9b49e52
jinpeng <jinpeng93@163.com>
2017-10-18 12:32:32 +0800

    1st case for Python

---
+++ case1/class1.py
@@ -0,0 +1,10 @@
+# -*- coding: UTF-8 -*-
+count = 0;
+for i in range(1, 5):
+	for j in range(1, 5):
+		for k in range(1, 5):
+			for l in range(1, 5):
+				if (i != k) and (i != j) and (j != k):
+					print(i,j,k)
+					count += 1
+print(count)
